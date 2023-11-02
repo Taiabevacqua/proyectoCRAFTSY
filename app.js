56e6188888298cf3eb03ba55a539611313441348
Taiabevacqua <natalia.bevacqua@gmail.com>
2023-11-02 09:21:36 -0300

    subiendo cambios formulario

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const PORT = 3030;
 
 app.use(express.static(path.join(__dirname,'/public')));
 
-
+/*rutas*/
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+
 
 
 app.listen(3030, () => console.log(`Server running in http://localhost:${PORT}`))
